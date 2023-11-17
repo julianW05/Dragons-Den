87d5ee808ddc91c83a1a174d9bccac2f07566ede
cem26 <cem@iagroup.app>
2023-11-17 13:38:09 +0100

    ziekenhuis pagina melding frontend en login button selectie

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,51 +1,88 @@
-import { Outlet, NavLink, useNavigate } from "react-router-dom";
-import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
-import {db, analytics, provider} from '../Firebase-config'
-import { useEffect, useState } from 'react'
-import sign_in from '../functions/Google-signin'
-import sign_out from '../functions/Google-signout'
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Outlet, useNavigate } from 'react-router-dom';
+import { collection, query, where, getDocs, addDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { db } from '../Firebase-config';
+import { useEffect, useState } from 'react';
+import sign_in from '../functions/Google-signin';
 
 export default function LoginLayout() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [userRole, setUserRole] = useState(null);
 
-  const authUser = async () => {
-    onAuthStateChanged(auth, async (userData) => {
+const authUser = () => {
+  return onAuthStateChanged(auth, async (userData) => {
+    setLoading(true);
+    try {
       if (userData) {
-        const q = query(collection(db, "users"), where("uid", "==", userData.uid));
+        const q = query(collection(db, 'users'), where('uid', '==', userData.uid));
         const querySnapshot = await getDocs(q);
+
         if (querySnapshot.empty) {
-          const docRef = await addDoc(collection(db, "users"), {
+          const docRef = await addDoc(collection(db, 'users'), {
             uid: userData.uid,
-            rol: "ziekenhuis",
-            isadmin: false
+            rol: null, 
+            isadmin: false,
           });
-          navigate('/ziekenhuis');
         } else {
           querySnapshot.forEach(async (doc) => {
-            if (doc.data().rol === "ziekenhuis") {
-              navigate('/ziekenhuis');
-            } else if (doc.data().rol === "ambulance") {
-              navigate('/ambulance');
-            }
+            const role = doc.data().rol;
+            setUserRole(role);
           });
         }
-        console.log(userData);
-      }});
-  }
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  });
+};
+
+
+  const handleRoleSelection = async (selectedRole) => {
+    try {
+      if (userRole === null) {
+        const q = query(collection(db, 'users'), where('uid', '==', auth.currentUser.uid));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach(async (doc) => {
+            await doc.ref.update({ rol: selectedRole });
+          });
+        }
+      }
+
+      setUserRole(selectedRole);
+
+      switch (selectedRole) {
+        case 'ziekenhuis':
+          navigate('/ziekenhuis');
+          break;
+        case 'ambulance':
+          navigate('/ambulance');
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
-    authUser();
-  }, [])
+    const unsubscribe = authUser();
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className="login row">
       <div className="background-image">
         <img src="https://media.discordapp.net/attachments/894538357793767474/1174264146137714739/image.png?ex=6566f5b5&is=655480b5&hm=c50c3aede1a28b925db2c94dfc47932af6dd4adc4e728549bc5c83f4124d653b&=&width=1051&height=662" alt="map" />
       </div>
-      <div className="left col-md-6">
-      </div>
+      <div className="left col-md-6"></div>
       <div className="right col-md-6">
         <img className="logo" src="https://media.discordapp.net/attachments/894538357793767474/1174268475204710490/ambulance-logo-free-vector.jpg?ex=6566f9bd&is=655484bd&hm=83480abfb581e42586e720dd9094506c700c05b2eee384b616a1dcc2cd87ed58&=&width=978&height=388" alt="logo" />
         <div className="title">
@@ -53,10 +90,18 @@ export default function LoginLayout() {
           <p>Log in met uw medisch account</p>
         </div>
         <div className="login-button col-md-5">
-            <button className="col-md-12" onClick={sign_in}>Log in</button>
+          <button className="col-md-12" onClick={sign_in}>
+            Log in
+          </button>
         </div>
+        {userRole === null && (
+          <div className="role-selection">
+            <button onClick={() => handleRoleSelection('ziekenhuis')}>Ziekenhuis medewerker</button>
+            <button onClick={() => handleRoleSelection('ambulance')}>Ambulance bestuurder</button>
+          </div>
+        )}
       </div>
-          <Outlet />
+      <Outlet />
     </div>
-  )
+  );
 }
--- src/layouts/ZiekenhuisLayout.jsx
+++ src/layouts/ZiekenhuisLayout.jsx
@@ -5,31 +5,78 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../Firebase-config";
 
 export default function ZiekenhuisLayout() {
-    const auth = getAuth();
-    const navigate = useNavigate();
+  const auth = getAuth();
+  const navigate = useNavigate();
 
-    const authUser = async () => {
-        onAuthStateChanged(auth, async (userData) => {
-          if (!userData) {
-            navigate('/');
-          } else if (userData) {
-            const q = query(collection(db, "users"), where("uid", "==", userData.uid));
-            const querySnapshot = await getDocs(q);
-            
-            querySnapshot.forEach(async (doc) => {
-                if (doc.data().rol === "ziekenhuis") {
-                navigate('/ziekenhuis');
-                } else if (doc.data().rol === "ambulance") {
-                navigate('/ambulance');
-                }
-            });
+  const authUser = async () => {
+    onAuthStateChanged(auth, async (userData) => {
+      if (!userData) {
+        navigate("/");
+      } else if (userData) {
+        const q = query(collection(db, "users"), where("uid", "==", userData.uid));
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach(async (doc) => {
+          if (doc.data().rol !== "ziekenhuis") {
+            navigate("/");
           }
         });
-      };
+      }
+    });
+  };
 
+  useEffect(() => {
+    authUser();
+  }, []);
 
-    useEffect(() => {
-        authUser();
-    }, [])
+  return (
+    <div className="ziekenhuis">
+      <header>
+        <div className="row">
+          <div className="logo">
+          <img src="https://media.discordapp.net/attachments/894538357793767474/1174268475204710490/ambulance-logo-free-vector.jpg?ex=6566f9bd&is=655484bd&hm=83480abfb581e42586e720dd9094506c700c05b2eee384b616a1dcc2cd87ed58&=&width=978&height=388" alt="logo" />
+          </div>
+          
+          <div className="cover">
+            <div className="meldingen">
+              {/* Your "meldingen" box goes here */}
+              {/* Example Form */}
+              <form >
+        <h2 >Melding</h2>
+
+          <label>
+            Ongeval:
+            <input type="text" placeholder="Ongeval" name="ongeval" />
+          </label>
+          <br />
+
+          <label>
+            Plaats:
+            <input type="text" placeholder="Plaats" name="plaats" />
+          </label>
+          <br />
+
+          <label>
+            Ambulance, classificatie:
+            <input type="text" placeholder="Ambulance, classificatie" name="ambulanceClassificatie" />
+          </label>
+          <br />
+
+          <label>
+            Situatie:
+            <textarea placeholder="Beschrijf de situatie..." name="iets" rows="1"></textarea>
+          </label>
+          <br />
+
+          <button type="submit">Stuur Melding</button>
+        </form>
+            </div>
+          </div>
+        </div>
+      </header>
+
     
+      <Outlet />
+    </div>
+  );
 }
